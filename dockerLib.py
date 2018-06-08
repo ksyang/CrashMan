af4c1f9047bd7d572b2517b43734623cdd78a3f4
ssgskid <ks8171235@naver.com>
2018-06-08 08:15:49 -0700

    init pingLib

--- dockerLib.py
+++ dockerLib.py
@@ -2,4 +2,4 @@ from os
 
 def dockerInit(dockerName, fuzzingProgram, fuzzerName, dockerRepo, hostIP):
 	os.system("docker pull %s:latest" % dockerRepo)
-	os.system("docker run -d -i --name %s %s './start.sh;python CrashMan_client/sender/sender.py /tmp/outputs/crashes %s %s %s'" % (dockerName, dockerRepo, fuzzerName, fuzzingProgram, hostIP))
+	os.system("docker run -d --privileged --name %s %s './start.sh %s %s %s %s'" % (dockerName, dockerRepo, fuzzerName, fuzzingProgram, hostIP, alias))
