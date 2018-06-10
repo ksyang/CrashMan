6efab8a0828bac7e1d313bcaf74c45387376c90b
ssgskid <ks8171235@naver.com>
2018-06-10 07:22:58 -0700

    modify easy_set script

--- easy_set.sh
+++ easy_set.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd ~
 export LC_ALL=C
-apt-get install -y python python-pip python3 python3-pip ufw git mysql-client mysql-server libmysqlclient-dev
+apt-get install -y python python-pip python3 python3-pip ufw git mysql-client mysql-server libmysqlclient-dev curl
+curl -fsSL https://get.docker.com/ | sudo sh
 pip3 install django pymysql mysqlclient
 pip install flask flask-mysql
+mysql -uroot -p -e "create database crashMan"
